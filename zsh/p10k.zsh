695a96530909d5eaaa52000ae0e242d7bc5276f7
mcpower <mcpower@users.noreply.github.com>
2026-04-06 20:32:00 +1000

    zsh: replace p10k git prompt with jj prompt + async.zsh

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -32,7 +32,7 @@
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # os_icon               # os identifier
     dir                     # current directory
-    vcs                     # git status
+    jj
     prompt_char             # prompt symbol
   )
 
@@ -278,169 +278,6 @@
   # Custom prefix.
   # typeset -g POWERLEVEL9K_DIR_PREFIX='%fin '
 
-  #####################################[ vcs: git status ]######################################
-  # Branch icon. Set this parameter to '\UE0A0 ' for the popular Powerline branch icon.
-  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=
-
-  # Untracked files icon. It's really a question mark, your font isn't broken.
-  # Change the value of this parameter to show a different icon.
-  typeset -g POWERLEVEL9K_VCS_UNTRACKED_ICON='?'
-
-  # Formatter for Git status.
-  #
-  # Example output: master wip ⇣42⇡42 *42 merge ~42 +42 !42 ?42.
-  #
-  # You can edit the function to customize how Git status looks.
-  #
-  # VCS_STATUS_* parameters are set by gitstatus plugin. See reference:
-  # https://github.com/romkatv/gitstatus/blob/master/gitstatus.plugin.zsh.
-  function my_git_formatter() {
-    emulate -L zsh
-
-    if [[ -n $P9K_CONTENT ]]; then
-      # If P9K_CONTENT is not empty, use it. It's either "loading" or from vcs_info (not from
-      # gitstatus plugin). VCS_STATUS_* parameters are not available in this case.
-      typeset -g my_git_format=$P9K_CONTENT
-      return
-    fi
-
-    if (( $1 )); then
-      # Styling for up-to-date Git status.
-      local       meta='%f'     # default foreground
-      local      clean='%76F'   # green foreground
-      local   modified='%178F'  # yellow foreground
-      local  untracked='%39F'   # blue foreground
-      local conflicted='%196F'  # red foreground
-    else
-      # Styling for incomplete and stale Git status.
-      local       meta='%244F'  # grey foreground
-      local      clean='%244F'  # grey foreground
-      local   modified='%244F'  # grey foreground
-      local  untracked='%244F'  # grey foreground
-      local conflicted='%244F'  # grey foreground
-    fi
-
-    local res
-
-    if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
-      local branch=${(V)VCS_STATUS_LOCAL_BRANCH}
-      # If local branch name is at most 32 characters long, show it in full.
-      # Otherwise show the first 12 … the last 12.
-      # Tip: To always show local branch name in full without truncation, delete the next line.
-      (( $#branch > 32 )) && branch[13,-13]="…"  # <-- this line
-      res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}${branch//\%/%%}"
-    fi
-
-    if [[ -n $VCS_STATUS_TAG
-          # Show tag only if not on a branch.
-          # Tip: To always show tag, delete the next line.
-          && -z $VCS_STATUS_LOCAL_BRANCH  # <-- this line
-        ]]; then
-      local tag=${(V)VCS_STATUS_TAG}
-      # If tag name is at most 32 characters long, show it in full.
-      # Otherwise show the first 12 … the last 12.
-      # Tip: To always show tag name in full without truncation, delete the next line.
-      (( $#tag > 32 )) && tag[13,-13]="…"  # <-- this line
-      res+="${meta}#${clean}${tag//\%/%%}"
-    fi
-
-    # Display the current Git commit if there is no branch and no tag.
-    # Tip: To always display the current Git commit, delete the next line.
-    [[ -z $VCS_STATUS_LOCAL_BRANCH && -z $VCS_STATUS_TAG ]] &&  # <-- this line
-      res+="${meta}@${clean}${VCS_STATUS_COMMIT[1,8]}"
-
-    # Show tracking branch name if it differs from local branch.
-    if [[ -n ${VCS_STATUS_REMOTE_BRANCH:#$VCS_STATUS_LOCAL_BRANCH} ]]; then
-      res+="${meta}:${clean}${(V)VCS_STATUS_REMOTE_BRANCH//\%/%%}"
-    fi
-
-    # Display "wip" if the latest commit's summary contains "wip" or "WIP".
-    if [[ $VCS_STATUS_COMMIT_SUMMARY == (|*[^[:alnum:]])(wip|WIP)(|[^[:alnum:]]*) ]]; then
-      res+=" ${modified}wip"
-    fi
-
-    if (( VCS_STATUS_COMMITS_AHEAD || VCS_STATUS_COMMITS_BEHIND )); then
-      # ⇣42 if behind the remote.
-      (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${clean}⇣${VCS_STATUS_COMMITS_BEHIND}"
-      # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
-      (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && res+=" "
-      (( VCS_STATUS_COMMITS_AHEAD  )) && res+="${clean}⇡${VCS_STATUS_COMMITS_AHEAD}"
-    elif [[ -n $VCS_STATUS_REMOTE_BRANCH ]]; then
-      # Tip: Uncomment the next line to display '=' if up to date with the remote.
-      # res+=" ${clean}="
-    fi
-
-    # ⇠42 if behind the push remote.
-    (( VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" ${clean}⇠${VCS_STATUS_PUSH_COMMITS_BEHIND}"
-    (( VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" "
-    # ⇢42 if ahead of the push remote; no leading space if also behind: ⇠42⇢42.
-    (( VCS_STATUS_PUSH_COMMITS_AHEAD  )) && res+="${clean}⇢${VCS_STATUS_PUSH_COMMITS_AHEAD}"
-    # *42 if have stashes.
-    (( VCS_STATUS_STASHES        )) && res+=" ${clean}*${VCS_STATUS_STASHES}"
-    # 'merge' if the repo is in an unusual state.
-    [[ -n $VCS_STATUS_ACTION     ]] && res+=" ${conflicted}${VCS_STATUS_ACTION}"
-    # ~42 if have merge conflicts.
-    (( VCS_STATUS_NUM_CONFLICTED )) && res+=" ${conflicted}~${VCS_STATUS_NUM_CONFLICTED}"
-    # +42 if have staged changes.
-    (( VCS_STATUS_NUM_STAGED     )) && res+=" ${modified}+${VCS_STATUS_NUM_STAGED}"
-    # !42 if have unstaged changes.
-    (( VCS_STATUS_NUM_UNSTAGED   )) && res+=" ${modified}!${VCS_STATUS_NUM_UNSTAGED}"
-    # ?42 if have untracked files. It's really a question mark, your font isn't broken.
-    # See POWERLEVEL9K_VCS_UNTRACKED_ICON above if you want to use a different icon.
-    # Remove the next line if you don't want to see untracked files at all.
-    (( VCS_STATUS_NUM_UNTRACKED  )) && res+=" ${untracked}${(g::)POWERLEVEL9K_VCS_UNTRACKED_ICON}${VCS_STATUS_NUM_UNTRACKED}"
-    # "─" if the number of unstaged files is unknown. This can happen due to
-    # POWERLEVEL9K_VCS_MAX_INDEX_SIZE_DIRTY (see below) being set to a non-negative number lower
-    # than the number of files in the Git index, or due to bash.showDirtyState being set to false
-    # in the repository config. The number of staged and untracked files may also be unknown
-    # in this case.
-    (( VCS_STATUS_HAS_UNSTAGED == -1 )) && res+=" ${modified}─"
-
-    typeset -g my_git_format=$res
-  }
-  functions -M my_git_formatter 2>/dev/null
-
-  # Don't count the number of unstaged, untracked and conflicted files in Git repositories with
-  # more than this many files in the index. Negative value means infinity.
-  #
-  # If you are working in Git repositories with tens of millions of files and seeing performance
-  # sagging, try setting POWERLEVEL9K_VCS_MAX_INDEX_SIZE_DIRTY to a number lower than the output
-  # of `git ls-files | wc -l`. Alternatively, add `bash.showDirtyState = false` to the repository's
-  # config: `git config bash.showDirtyState false`.
-  typeset -g POWERLEVEL9K_VCS_MAX_INDEX_SIZE_DIRTY=-1
-
-  # Don't show Git status in prompt for repositories whose workdir matches this pattern.
-  # For example, if set to '~', the Git repository at $HOME/.git will be ignored.
-  # Multiple patterns can be combined with '|': '~(|/foo)|/bar/baz/*'.
-  typeset -g POWERLEVEL9K_VCS_DISABLED_WORKDIR_PATTERN='~'
-
-  # Disable the default Git status formatting.
-  typeset -g POWERLEVEL9K_VCS_DISABLE_GITSTATUS_FORMATTING=true
-  # Install our own Git status formatter.
-  typeset -g POWERLEVEL9K_VCS_CONTENT_EXPANSION='${$((my_git_formatter(1)))+${my_git_format}}'
-  typeset -g POWERLEVEL9K_VCS_LOADING_CONTENT_EXPANSION='${$((my_git_formatter(0)))+${my_git_format}}'
-  # Enable counters for staged, unstaged, etc.
-  typeset -g POWERLEVEL9K_VCS_{STAGED,UNSTAGED,UNTRACKED,CONFLICTED,COMMITS_AHEAD,COMMITS_BEHIND}_MAX_NUM=-1
-
-  # Icon color.
-  typeset -g POWERLEVEL9K_VCS_VISUAL_IDENTIFIER_COLOR=76
-  typeset -g POWERLEVEL9K_VCS_LOADING_VISUAL_IDENTIFIER_COLOR=244
-  # Custom icon.
-  typeset -g POWERLEVEL9K_VCS_VISUAL_IDENTIFIER_EXPANSION=
-  # Custom prefix.
-  # typeset -g POWERLEVEL9K_VCS_PREFIX='%fon '
-
-  # Show status of repositories of these types. You can add svn and/or hg if you are
-  # using them. If you do, your prompt may become slow even when your current directory
-  # isn't in an svn or hg repository.
-  typeset -g POWERLEVEL9K_VCS_BACKENDS=(git)
-
-  # These settings are used for repositories other than Git or when gitstatusd fails and
-  # Powerlevel10k has to fall back to using vcs_info.
-  typeset -g POWERLEVEL9K_VCS_CLEAN_FOREGROUND=76
-  typeset -g POWERLEVEL9K_VCS_UNTRACKED_FOREGROUND=76
-  typeset -g POWERLEVEL9K_VCS_MODIFIED_FOREGROUND=178
-
   ##########################[ status: exit code of the last command ]###########################
   # Enable OK_PIPE, ERROR_PIPE and ERROR_SIGNAL status states to allow us to enable, disable and
   # style them independently from the regular OK and ERROR state.
@@ -541,6 +378,92 @@
   typeset -g POWERLEVEL9K_VIRTUALENV_{LEFT,RIGHT}_DELIMITER=
   # Custom icon.
   # typeset -g POWERLEVEL9K_VIRTUALENV_VISUAL_IDENTIFIER_EXPANSION='⭐'
+
+  # Based on romkatv's comment: https://old.reddit.com/r/zsh/comments/l2y976/_/gkb9y72/
+  # and Pure's use of async.zsh for their prompt: https://github.com/sindresorhus/pure/blob/main/pure.zsh
+  function async_jj_op() {
+    jj -R "$1" --ignore-working-copy --at-op=@ op log -n 1 -T "id.short(12)" --no-graph 2> /dev/null
+  }
+  function async_jj_at() {
+    jj -R "$1" --ignore-working-copy --at-op=@ log --no-graph -r @ -T "diff.summary()" 2> /dev/null | awk 'BEGIN {a=0;d=0;m=0} /^A / {a++} /^D / {d++} /^M / {m++} /^R / {m++} /^C / {a++} END {print(a,d,m)}'
+  }
+  function async_jj_co() {
+    setopt localoptions pipefail
+    jj -R "$1" --ignore-working-copy --at-op=@ resolve --list 2> /dev/null | wc -l
+  }
+  local prompt_jj_last_root=
+  function async_prompt_jj_init() {
+    prompt_jj_last_root=
+    async_start_worker        prompt_jj -u -n
+    async_register_callback   prompt_jj async_prompt_jj_callback
+  }
+  function async_prompt_jj_run_jobs() {
+    if [[ "$1" != "$prompt_jj_last_root" ]]; then
+      typeset -g p10k_jj_co= p10k_jj_op="…loading op…" p10k_jj_at_add= p10k_jj_at_del= p10k_jj_at_mod=
+      prompt_jj_last_root="$1"
+      async_flush_jobs prompt_jj
+    fi
+    async_job prompt_jj async_jj_op "$1"
+    async_job prompt_jj async_jj_at "$1"
+    async_job prompt_jj async_jj_co "$1"
+  }
+  function async_prompt_jj_callback() {
+    setopt localoptions noshwordsplit extended_glob
+    local job=$1 code=$2 output=$3 exec_time=$4 next_pending=$6
+
+    case $job in
+      \[async])
+        if (( code == 2 )) || (( code == 3 )) || (( code == 130 )); then
+          async_stop_worker prompt_jj
+          async_prompt_jj_init
+          local roots=(./(../)#(.jj)(#qN/))
+          [[ -n $roots ]] && async_prompt_jj_run_jobs "${roots[-1]:h:a}"
+          return
+        fi
+        ;;
+      \[async/eval])
+        if (( code )); then
+          local roots=(./(../)#(.jj)(#qN/))
+          [[ -n $roots ]] && async_prompt_jj_run_jobs "${roots[-1]:h:a}"
+          return
+        fi
+        ;;
+      async_jj_op)
+        typeset -g p10k_jj_op= p10k_jj_op_stale= p10k_jj_op_updated=1
+        [[ $code -eq 0 ]] && typeset -g p10k_jj_op="$output"
+        ;;
+      async_jj_at)
+        typeset -g p10k_jj_at_add= p10k_jj_at_del= p10k_jj_at_mod= p10k_jj_at_stale= p10k_jj_at_updated=1
+        if [[ $code -eq 0 ]]; then
+          parts=(${(@s: :)output})
+          [[ ${parts[1]} != "0" ]] && typeset -g p10k_jj_at_add="+${parts[1]}"
+          [[ ${parts[2]} != "0" ]] && typeset -g p10k_jj_at_del="-${parts[2]}"
+          [[ ${parts[3]} != "0" ]] && typeset -g p10k_jj_at_mod="^${parts[3]}"
+        fi
+        ;;
+      async_jj_co)
+        typeset -g p10k_jj_co= p10k_jj_co_stale= p10k_jj_co_updated=1
+        [[ $code -eq 0 ]] && typeset -g p10k_jj_co="!$output"
+        ;;
+    esac
+    if ! (( next_pending )); then
+      p10k display -r
+    fi
+  }
+  async_prompt_jj_init
+  function prompt_jj() {
+    setopt localoptions extended_glob noshwordsplit
+    (( $+commands[jj] )) || return
+    local roots=(./(../)#(.jj)(#qN/))
+    [[ -n $roots ]] || return
+    typeset -g p10k_jj_op_stale=1 p10k_jj_op_updated= p10k_jj_at_stale=1 p10k_jj_at_updated= p10k_jj_co_stale=1 p10k_jj_co_updated=
+    async_prompt_jj_run_jobs "${roots[-1]:h:a}"
+    p10k segment -f grey -c '$p10k_jj_op_stale' -e -t '$p10k_jj_op';     p10k segment -f green  -c '$p10k_jj_op_updated' -e -t '$p10k_jj_op'
+    p10k segment -f grey -c '$p10k_jj_at_stale' -e -t '$p10k_jj_at_add'; p10k segment -f green  -c '$p10k_jj_at_updated' -e -t '$p10k_jj_at_add'
+    p10k segment -f grey -c '$p10k_jj_at_stale' -e -t '$p10k_jj_at_del'; p10k segment -f red    -c '$p10k_jj_at_updated' -e -t '$p10k_jj_at_del'
+    p10k segment -f grey -c '$p10k_jj_at_stale' -e -t '$p10k_jj_at_mod'; p10k segment -f yellow -c '$p10k_jj_at_updated' -e -t '$p10k_jj_at_mod'
+    p10k segment -f grey -c '$p10k_jj_co_stale' -e -t '$p10k_jj_co';     p10k segment -f 9      -c '$p10k_jj_co_updated' -e -t '$p10k_jj_co'
+  }
 
   # Transient prompt works similarly to the builtin transient_rprompt option. It trims down prompt
   # when accepting a command line. Supported values:
